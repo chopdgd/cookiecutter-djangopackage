833a4db82fd2f644bc68f94d25d1834495e6285a
genomics-geek <GonzalezMA@email.chop.edu>
2019-05-31 11:02:25 -0400

    Updated how urls are imported in the test settings section

--- {{cookiecutter.repo_name}}/tests/urls.py
+++ {{cookiecutter.repo_name}}/tests/urls.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
-
-from {{ cookiecutter.app_name }}.urls import urlpatterns as {{ cookiecutter.app_name }}_urls
 
 
 app_name = '{{ cookiecutter.app_name }}'
 urlpatterns = [
-    url(r'^', include({{ cookiecutter.app_name }}_urls)),
+    url(r'^', include('{{ cookiecutter.app_name }}.urls')),
     url(r'^admin/', admin.site.urls),
 ]
