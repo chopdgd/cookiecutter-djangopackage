7ee2dd927afe538486d8b038dadeee88b8e6431f
genomics-geek <GonzalezMA@email.chop.edu>
2019-05-30 20:12:47 -0400

    Updated urls.py to support django 2.1+

--- {{cookiecutter.repo_name}}/tests/urls.py
+++ {{cookiecutter.repo_name}}/tests/urls.py
@@ -6,7 +6,9 @@ from django.contrib import admin
 
 from {{ cookiecutter.app_name }}.urls import urlpatterns as {{ cookiecutter.app_name }}_urls
 
+
+app_name = '{{ cookiecutter.app_name }}'
 urlpatterns = [
-    url(r'^', include({{ cookiecutter.app_name }}_urls, namespace='{{ cookiecutter.app_name }}')),
+    url(r'^', include({{ cookiecutter.app_name }}_urls)),
     url(r'^admin/', admin.site.urls),
 ]
